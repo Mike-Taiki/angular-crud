285b12a24faff0f80d649d2aa8fd5e243bb0e71f
Michael Gomes <michael.21.2012@hotmail.com>
2019-12-07 10:30:43 -0300

    created design

--- productsapp/src/app/department/department.component.ts
+++ productsapp/src/app/department/department.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Department } from "./../department";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-department',
-  templateUrl: './department.component.html',
-  styleUrls: ['./department.component.scss']
+  selector: "app-department",
+  templateUrl: "./department.component.html",
+  styleUrls: ["./department.component.scss"]
 })
 export class DepartmentComponent implements OnInit {
+  depName: string = "";
+  departments: Department[] = [
+    { name: "dep1", _id: 1 },
+    { name: "dep2", _id: 1 },
+    { name: "dep3", _id: 1 },
+    { name: "dep4", _id: 1 }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  save() {}
+
+  cancel() {}
+
+  edit(dep: Department) {}
+
+  delete(dep: Department) {}
 }
