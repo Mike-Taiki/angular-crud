ab971acd7dd63ca270ab740e8199b70de7075460
Michael Gomes <michael.21.2012@hotmail.com>
2019-12-08 19:41:22 -0300

    created product routes

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const department_controller = require("./department_controller");
+const product_controller = require("./product_controller");
 
 const app = express();
 
@@ -16,7 +17,7 @@ mongoose.connect("mongodb://localhost:27017/http_app", {
 });
 
 app.use("/departments", department_controller);
-// app.use("/products", product_controller);
+app.use("/products", product_controller);
 
 const PORT = process.env.PORT || 3000;
 
